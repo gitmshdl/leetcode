c193fc35ca792680052b52e35d545ce8c61032e1
Seung Hwan (David) Lee <50871680+gitmshdl@users.noreply.github.com>
2023-07-18 04:42:39 -0400

    Create 27. Remove Element.js

---
+++ 27. Remove Element.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    if (!nums) return 0
+    for (let i=0;i<nums.length;i++){
+        if (nums[i] === val) {
+            nums.splice(i,1)
+            i--
+        }
+    }
+    return nums.length
+};
+
+// const removeElement = function(nums,val) {
+//     if (!nums) return 0
+//     let pointer=0
+
+//     for(let i=0;i<nums.length;i++){
+//         if (nums[i]!==val ) {
+//             nums[pointer]=nums[i]
+//             pointer++
+//         } 
+//     }return pointer
+// }
