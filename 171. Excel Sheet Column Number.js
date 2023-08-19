cd2575d5dcac19796db526167b6f282e8f9c1fb5
Seung Hwan (David) Lee <50871680+gitmshdl@users.noreply.github.com>
2023-08-18 21:34:24 -0400

    Create 171. Excel Sheet Column Number.js

---
+++ 171. Excel Sheet Column Number.js
@@ -0,0 +1,7 @@
+var titleToNumber = function(columnTitle) {
+    let sum=0
+    for (let i=0;i<columnTitle.length;i++){
+        sum=(sum*26)+(columnTitle.charCodeAt(i)-64)
+    }
+    return sum
+};
